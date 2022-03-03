0c22c3c6ddc2bdbe34c1d3915dd24e086136053b
Sophia <Sophia@DESKTOP-KGK1V51>
2022-03-03 20:36:11 +0900

    20220303 BaekJoon11399, ATM(실3)

---
+++ CodingTest/src/BaekJoon/BaekJoon11399.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon11399 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), sum=0, result=0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int arr[] = new int[N];
+		for(int i=0; st.hasMoreTokens(); i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i=0; i<N; i++) {
+			sum = sum + arr[i];
+			result += sum;
+		}
+		System.out.println(result);
+	}
+
+}
