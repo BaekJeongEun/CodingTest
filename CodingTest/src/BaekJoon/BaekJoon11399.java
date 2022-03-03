eccd5f337e4aee86afdfbeafb3c5460567e42c65
Sophia <Sophia@DESKTOP-KGK1V51>
2022-03-03 20:38:32 +0900

    20200303 BaekJoon11399 ATM(실3)

--- CodingTest/src/BaekJoon/BaekJoon11399.java
+++ CodingTest/src/BaekJoon/BaekJoon11399.java
@@ -14,14 +14,14 @@ public class BaekJoon11399 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int arr[] = new int[N];
 		for(int i=0; st.hasMoreTokens(); i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken()); // 값 입력
 		}
-		Arrays.sort(arr);
+		Arrays.sort(arr); // 오름차순 정렬
 		for(int i=0; i<N; i++) {
-			sum = sum + arr[i];
-			result += sum;
+			sum = sum + arr[i];	// 현재 사람이 기다린 시간
+			result += sum;		// 누적 시간
 		}
-		System.out.println(result);
+		System.out.println(result); // 누적 시간 출력
 	}
 
 }
