ef4e6b8cc3309f515def849cd649491deed8ee38
Sophia <Sophia@DESKTOP-KGK1V51>
2022-03-06 23:53:10 +0900

    20220306 BaekJoon11654 아스키 코드 브1

---
+++ CodingTest/src/BaekJoon/BaekJoon11654.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon11654 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().getBytes()[0]);
+	}
+
+}
