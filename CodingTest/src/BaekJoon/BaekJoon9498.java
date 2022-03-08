ab0858d516a7f536cdcfcaa25ff7a81cabc5a4a0
Sophia <Sophia@DESKTOP-KGK1V51>
2022-03-08 17:58:05 +0900

    20220308 BaekJoon9498 시험 성적, 브4

---
+++ CodingTest/src/BaekJoon/BaekJoon9498.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon9498 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int score = Integer.parseInt(br.readLine());
+		char result = 'a';
+		if(score>=90) {
+			result = 'A';
+		}else if(score>=80) {
+			result = 'B';
+		}else if(score>=70) {
+			result = 'C';
+		}else if(score>=60) {
+			result = 'D';
+		}else
+			result = 'F';
+		System.out.println(result);
+	}
+}
