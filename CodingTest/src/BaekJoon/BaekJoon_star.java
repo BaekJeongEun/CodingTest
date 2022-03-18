b6c57b23dbdf9d9c695d6e554f93cf9c1081894e
BaekJeongEun <sophia991106@gmail.com>
2022-03-18 23:49:33 +0900

    20220318

--- CodingTest/src/BaekJoon/BaekJoon_star.java
+++ CodingTest/src/BaekJoon/BaekJoon_star.java
@@ -10,20 +10,24 @@ public class BaekJoon_star {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
+		for(int i=1; i<=9; i++) {
+			System.out.println(N+" * "+i+" = "+(N*i));
+		}
+		
 //		for(int i=0; i<N; i++) {
 //			for(int j=0; j<i+1; j++) {
 //				System.out.print("*");
 //			}
 //			System.out.println();
 //		}
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(j<N-i-1)
-					System.out.print(" ");
-				else
-					System.out.print("*");
-			}
-			System.out.println();
-		}
+//		for(int i=0; i<N; i++) {
+//			for(int j=0; j<N; j++) {
+//				if(j<N-i-1)
+//					System.out.print(" ");
+//				else
+//					System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 	}
 }
