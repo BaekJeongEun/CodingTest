e68fa6637c20ea59ca768687f765d4d46fd423bf
BaekJeongEun <sophia991106@gmail.com>
2022-03-19 23:32:50 +0900

    20220319

---
+++ CodingTest/src/BaekJoon/BaekJoonBronze.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoonBronze {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine()+"??!");
+	}
+}
