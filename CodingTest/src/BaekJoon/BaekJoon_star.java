06328bd16416305cec0a3a15ecc854de9a237589
BaekJeongEun <sophia991106@gmail.com>
2022-03-29 23:57:07 +0900

    20220329

--- CodingTest/src/BaekJoon/BaekJoon_star.java
+++ CodingTest/src/BaekJoon/BaekJoon_star.java
@@ -3,17 +3,23 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BaekJoon_star {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double a = Double.parseDouble(st.nextToken());
+		double b = Double.parseDouble(st.nextToken());
+		System.out.println(a/b);
 		
-		for(int i=1; i<=N; i++) {
-			System.out.println(i);
-		}
-		
+//		int N = Integer.parseInt(br.readLine());
+//		
+//		for(int i=1; i<=N; i++) {
+//			System.out.println(i);
+//		}
+//		
 //		for(int i=0; i<N; i++) {
 //			for(int j=0; j<i+1; j++) {
 //				System.out.print("*");
