6fb330e19f2d33668af410099347847dccc617fa
BaekJeongEun <sophia991106@gmail.com>
2022-04-26 23:06:51 +0900

    20200426 최소, 최대

---
+++ CodingTest/src/BaekJoon/BaekJoon10818.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon10818 {
+
+	public static void main(String[] args)  throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), min=10000001, max=0;
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if(min>arr[i])
+				min = arr[i];
+			if(max<arr[i])
+				max = arr[i];
+		}
+		System.out.println(min+" "+max);
+	}
+
+}
