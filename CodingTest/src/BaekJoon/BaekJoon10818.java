7070257d7baab0551c05b08fe2e12caaa5cae8e5
BaekJeongEun <sophia991106@gmail.com>
2022-04-28 23:28:38 +0900

    20220428

--- CodingTest/src/BaekJoon/BaekJoon10818.java
+++ CodingTest/src/BaekJoon/BaekJoon10818.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BaekJoon10818 {
-
 	public static void main(String[] args)  throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()), min=10000001, max=0;
