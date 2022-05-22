ebab50ebb8c7b41d2c36385df0518ed8e1651a00
BaekJeongEun <sophia991106@gmail.com>
2022-05-23 00:41:28 +0900

    20220523

--- CodingTest/src/Programmers/Main.java
+++ CodingTest/src/Programmers/Main.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class 호텔방배정 {
+public class Main {
 	static HashMap<Long, Long> hashRoom =  new HashMap<>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
