e3b4821d208c4e17a41d0a1a70da0539e5ddd3c3
BaekJeongEun <sophia991106@gmail.com>
2022-05-24 22:44:49 +0900

    220524

---
+++ CodingTest/src/Programmers/PlayerClothes.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+public class PlayerClothes {
+
+	public static void main(String[] args) {
+		int lost[] = {2,4};
+		int reserve[] = {1,3,5};
+		System.out.println(solution(5, lost, reserve));
+	}
+
+	static public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+}
