824d02215acb3cd7b9799ef145dfe9318dd51ee9
BaekJeongEun <sophia991106@gmail.com>
2022-05-25 12:59:30 +0900

    220525

--- CodingTest/src/Programmers/PlayerClothes.java
+++ CodingTest/src/Programmers/PlayerClothes.java
@@ -1,5 +1,7 @@
 package Programmers;
 
+import java.util.HashSet;
+
 public class PlayerClothes {
 
 	public static void main(String[] args) {
@@ -9,7 +11,26 @@ public class PlayerClothes {
 	}
 
 	static public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        return answer;
+		HashSet<Integer> lostSet = new HashSet<>();
+		HashSet<Integer> reserveSet = new HashSet<>();
+		// 체육복 남는 인원
+		for(int i : reserve)
+			reserveSet.add(i);
+		// 체육복 도난 당한 인원
+		for(int i : lost) {
+			if(reserveSet.contains(i)) // 체육복 남는 인원이 도난 당했을 경우
+				reserveSet.remove(i); // 내 체육복만 남게 됨
+			else
+				lostSet.add(i);
+		}
+		// 체육복 남는 인원 중 양 옆 사람이 도난 당한 사람일 경우 빌려주기
+		for(int i : reserveSet) {
+			if(lostSet.contains(i-1))
+				lostSet.remove(i-i);
+			else if(lostSet.contains(i+1))
+				lostSet.remove(i+1);
+		}
+		
+        return n-lostSet.size();
     }
 }
