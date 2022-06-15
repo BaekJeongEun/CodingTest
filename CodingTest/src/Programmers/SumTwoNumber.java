518ec794640dd68fb9712b3a7234ce6d01b22916
BaekJeongEun <sophia991106@gmail.com>
2022-06-15 17:37:25 +0900

    220615

---
+++ CodingTest/src/Programmers/SumTwoNumber.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SumTwoNumber {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution(new int[] {2,1,3,4,1})));
+	}
+
+	public static int[] solution(int[] numbers) {
+        int[] answer = {};
+
+        Set<Integer> set = new TreeSet<>();
+        for(int i=0; i<numbers.length; i++){
+        	for(int j=1; j<numbers.length; j++) {
+        		if(i!=j)
+        			set.add(numbers[i]+numbers[j]);
+        	}
+        }
+        answer = set.stream().mapToInt(Number::intValue).toArray();
+        return answer;
+    }
+}
