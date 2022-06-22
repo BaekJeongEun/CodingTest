abe8f57708bb2c58f00cfed788463116791f8dbb
BaekJeongEun <sophia991106@gmail.com>
2022-06-22 18:14:53 +0900

    220622

--- CodingTest/src/Programmers/Hiding.java
+++ CodingTest/src/Programmers/Hiding.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 public class Hiding {
 
 	public static void main(String[] args) {
-		System.out.println(solution(new String[][]{{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}}));
+		System.out.println(solution(new String[][]{{"yellowhat"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}}));
 
 	}
 	public static int solution(String[][] clothes) {
