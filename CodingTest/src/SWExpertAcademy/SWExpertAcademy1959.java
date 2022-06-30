d8ffef2a7dd7590d4e79f6e7bda39faf0aa2cf0a
BaekJeongEun <sophia991106@gmail.com>
2022-07-01 00:09:24 +0900

    220701 두 개의 숫자열

--- CodingTest/src/SWExpertAcademy/SWExpertAcademy1959.java
+++ CodingTest/src/SWExpertAcademy/SWExpertAcademy1959.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class SWExpertAcademy1959 {
+public class SWExpertAcademy1959 { // 두 개의 숫자열
 
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
