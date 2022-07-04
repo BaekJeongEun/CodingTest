89a92449e6c4bb9c253fdfa509c24a4cac9b7541
BaekJeongEun <sophia991106@gmail.com>
2022-07-04 16:54:40 +0900

    220704

--- CodingTest/src/Programmers/Programmers124CountryNum.java
+++ CodingTest/src/Programmers/Programmers124CountryNum.java
@@ -1,6 +1,6 @@
 package Programmers;
 
-public class Programmers124CountryNum {
+public class Programmers124CountryNum { // 124 나라
 
 	public static void main(String[] args) {
 		System.out.println(solution(4));
