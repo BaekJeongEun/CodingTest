4bdf3ffd97f5dc7b1d8001f5117aa5280ed7b2d2
BaekJeongEun <sophia991106@gmail.com>
2022-07-07 00:57:21 +0900

    220707 단체사진 찍기

--- CodingTest/src/Programmers/TakePicture.java
+++ CodingTest/src/Programmers/TakePicture.java
@@ -5,48 +5,51 @@ public class TakePicture {
 	static String member = "ACFJMNRT"; 
 	static boolean visited[];
 	static String[] dataList;
-	static int total;
+	static int total, count;
 	public static void main(String[] args) {
 		System.out.println(solution(2, new String[] {"N~F=0", "R~T>2"}));
-
 	}
 	public static int solution(int n, String[] data) {
         N = n;
+        
+        visited = new boolean[8];
+        dataList = new String[data.length];
+        
         dataList = data;
-        String hello = "";
-        dfs(hello);
+        dfs("");
         return total;
     }
 	
-	public static void dfs(String s) {
-		if(s.length() == 8) {
-			System.out.println("s "+s);
-			check(s);
-			
+	public static void dfs(String names) {
+		if(names.length() == 7) {
+			if(check(names))
+				total++;
 			return;
 		}
-		System.out.println("sss "+s);
+		System.out.println("S "+names);
 		for(int i=0; i<8; i++) {
-			System.out.println("member "+member.indexOf(i)+"     ? "+member);
-			if(!visited[member.indexOf(i)]) {
-				visited[member.indexOf(i)] = true;
-				s += member.indexOf(i)+"";
-				System.out.println("s111 "+s);
-				dfs(s);
-				visited[member.indexOf(i)] = false;
+			if(!visited[i]) {
+				visited[i] = true;
+				String name = names + member.charAt(i);
+				dfs(name);
+				visited[i] = false;
 			}
 		}
 	}
-	public static void check(String s) {
-		int temp = 0;
+	public static boolean check(String s) {
 		for(int i=0; i<N; i++) {
-			if(Math.abs(s.indexOf(dataList[i].indexOf(0)) - s.indexOf(dataList[i].indexOf(2)))
-					<= dataList[i].indexOf(4)) {
-				temp++;
-			}
+			int position1 = s.indexOf(dataList[i].charAt(0)); // 포지션1
+            int position2 = s.indexOf(dataList[i].charAt(2)); // 포지션2
+            char op = dataList[i].charAt(3); // 수식
+            int index = dataList[i].charAt(4) -'0'; // 간격
+            if (op == '=') {
+                if (!(Math.abs(position1 - position2) == index+1)) return false;
+            } else if (op == '>') {
+                if (!(Math.abs(position1 - position2) > index+1)) return false;
+            } else if (op == '<') {
+                if (!(Math.abs(position1 - position2) < index+1)) return false;
+            }
 		}
-		if(temp == N) {
-			total++;
-		}
+        return true;
 	}
 }
