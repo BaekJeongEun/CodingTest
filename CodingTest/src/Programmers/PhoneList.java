b3f47f274b665a187a650469bac07144b8bcd928
BaekJeongEun <sophia991106@gmail.com>
2022-07-07 21:46:33 +0900

    220707 전화번호 목록

---
+++ CodingTest/src/Programmers/PhoneList.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class PhoneList {
+
+	public static void main(String[] args) {
+		System.out.println(solution(new String[] { "12", "123", "1235", "567", "88" }));
+	}
+
+	public static boolean solution(String[] phone_book) {
+		boolean answer = true;
+
+		Arrays.sort(phone_book);
+
+		for (int i = 0; i < phone_book.length-1 && answer; i++) {
+			if(phone_book[i+1].startsWith(phone_book[i]))
+				answer = false;
+		}
+		return answer;
+	}
+}
