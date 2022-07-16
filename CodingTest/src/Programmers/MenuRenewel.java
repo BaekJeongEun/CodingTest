6ef1cf8133ef1fff3f38b8fcf7e4e651d4830f2f
BaekJeongEun <sophia991106@gmail.com>
2022-07-16 21:06:38 +0900

    [Programmers]: 메뉴 리뉴얼 (Lv2)

--- CodingTest/src/Programmers/MenuRenewel.java
+++ CodingTest/src/Programmers/MenuRenewel.java
@@ -1,52 +1,68 @@
 package Programmers;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 
 public class MenuRenewel {
+	static HashMap<String, Integer> map = new HashMap<>();
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(solution(new String[] {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[] {2,3,4})));
+		System.out.println(Arrays.toString(
+				solution(new String[] { "ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD" }, new int[] { 2, 3, 5 })));
 
 	}
+
 	public static String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        Arrays.sort(orders, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				// TODO Auto-generated method stub
-				return o1.length()-o2.length();
+		String[] answer = {};
+
+		List<String> list = new ArrayList<>();
+
+		for (int i = 0; i < orders.length; i++) {
+			char[] ch = orders[i].toCharArray();
+			Arrays.sort(ch);
+			orders[i] = String.valueOf(ch);
+		}
+		// 주문한 메뉴들을 course 길이만큼 조합
+		for (int c = 0; c < course.length; c++) {
+
+			for (int o = 0; o < orders.length; o++) {
+				if (orders[o].length() >= course[c]) {
+					// 조합된 문자열, 목표 조합 길이, 주어진 문자열, 조합될 문자 인덱스
+					//StringBuilder sb = new StringBuilder(); 
+					comb(orders[o],"",0,0,course[c]);
+				}
 			}
-		});
-        
-        for(int j=0; j<orders.length; j++) {
-        	
-        	String target=".";
-        	for(int i=0; i<course.length; i++) {
-        		if(course[i] == orders[j].length()) {
-        			target = orders[j];
-        			continue;
-        		}
-        		if(orders[j].contains(target)) {
-        			System.out.println("target "+ target+"  orders "+orders[j]);
-        			map.put(orders[j], map.getOrDefault(orders[j], 0)+1);
-        		}
-        	}
-        }
-        for(String key : map.keySet()) {
-        	if(map.get(key)<=1) {
-        		map.remove(key);
-        	}
-        }
-        answer = new String[map.size()];
-        int i=0;
-        for(String key : map.keySet()) {
-        	answer[i++] = key;
-        }
-        return answer;
-    }
+			// map에 저장된 조합된 메뉴들 중 최댓값만 추출
+			if (!map.isEmpty()) {
+				List<Integer> menu = new ArrayList<>(map.values());
+				int max = Collections.max(menu);
+
+				System.out.println("course[c] "+course[c]);
+				System.out.println("max "+max);
+				if (max > 1) {
+					for (String s : map.keySet()) {
+						if (map.get(s) == max) {
+							list.add(s);
+						}
+					}
+				}
+				map.clear();
+			}
+		}
+
+		answer = list.toArray(new String[list.size()]);
+		Arrays.sort(answer);
+		return answer;
+	}
+
+	public static void comb(String str,String sb,int idx, int cnt, int n) {
+		if (cnt == n) {
+			map.put(sb,map.getOrDefault(sb,0)+1);
+			return;
+		}
+		for (int i = idx; i < str.length(); i++) {
+            sb += str.charAt(i);
+            comb(str,sb,i+1,cnt+1,n);
+            sb = sb.substring(0, sb.length() - 1);
+		}
+	}
 }
