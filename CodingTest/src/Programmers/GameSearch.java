5b58818982b64af5bdab5b1674c341ba922fd027
BaekJeongEun <sophia991106@gmail.com>
2022-07-19 23:39:11 +0900

    [Programmers]: 게임 맵 최단거리 (Lv2)

--- CodingTest/src/Programmers/GameSearch.java
+++ CodingTest/src/Programmers/GameSearch.java
@@ -3,33 +3,30 @@ package Programmers;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class GameSearch {
+public class GameSearch { // 게임 맵 최단거리
 	public static void main(String[] args) {
 		System.out.println("답 "+solution(new int[][] {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,0}, {0,0,0,0,1}}));
 		System.out.println("답 "+solution(new int[][] {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,1}, {0,0,0,0,1}}));
 
 	}
 	public static int solution(int[][] maps) {
-		int[] dx = {0,0,1,-1};
-		int[] dy = {1,-1,0,0};
-        int answer = 0;
-        boolean exit = false;
+		int[] dx = {0,1,0,-1};
+		int[] dy = {1,0,-1,0};
+        int answer = Integer.MAX_VALUE;
         Queue<Point> queue = new LinkedList<>();
-        int nowX = 0, nowY=0;
-        queue.add(new Point(0,0));
+        int nowX = 0, nowY=0, count=0;
+        queue.add(new Point(0,0,1));
         
-        while(!queue.isEmpty() && !exit) {
+        while(!queue.isEmpty()) {
         	
-        	int size = queue.size();
-        	answer++;
-        	System.out.println("size "+size);
-        	for(int s=0; s<size; s++) {
+        	//for(int s=0; s<size; s++) {
         		Point now = queue.poll();
             	nowX = now.x;
             	nowY = now.y;
+            	count = now.count;
             	
             	if(nowX == maps.length-1 && nowY == maps[0].length-1) {
-            		exit = true;
+            		answer = (answer > count) ? count : answer;
             		break;
             	}
             	for(int i=0; i<4; i++) {
@@ -37,21 +34,22 @@ public class GameSearch {
             		int nextY = nowY + dy[i];
             		System.out.println(nextX +" "+nextY);
             		if(nextX>=0 && nextX<= maps.length-1 && nextY>=0 && nextY<=maps[0].length-1 && maps[nextX][nextY]==1) {
-            			queue.add(new Point(nextX, nextY));
             			maps[nextX][nextY] = 0;
+            			queue.add(new Point(nextX, nextY, count+1));
             		}
             	}
-        	}
+        	//}
         }
         
-        return exit?answer:-1;
+        return (Integer.MAX_VALUE==answer)?-1:answer;
     }	
 	
 	public static class Point{
-		int x, y;
-		public Point(int x, int y) {
+		int x, y, count;
+		public Point(int x, int y, int count) {
 			this.x = x;
 			this.y = y;
+			this.count = count;
 		}
 	}
 }
