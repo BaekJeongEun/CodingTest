92b2ff8f603753419342ed0b7dc79d39696eaa7d
BaekJeongEun <sophia991106@gmail.com>
2022-07-21 14:21:34 +0900

    [BaekJoon]: N과 M (S3)

--- CodingTest/src/BaekJoon/_BaekJoon15649.java
+++ CodingTest/src/BaekJoon/_BaekJoon15649.java
@@ -7,6 +7,48 @@ import java.util.StringTokenizer;
 
 public class _BaekJoon15649 {
 	static int N, M, arr[], select[];
+	static boolean visit[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		visit = new boolean[N];
+		select = new int[M];
+		for(int i=0; i<N; i++) {
+			arr[i] = i+1;
+		}
+		permutation(0,0);
+	}
+	private static void permutation(int index, int cnt) {
+		if(cnt == M) {
+			for(int i=0; i<select.length; i++) {
+				System.out.print(select[i]+" ");
+			}System.out.println();
+			return;
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(!visit[i]) {
+				visit[i] = true;
+				select[cnt] = arr[i];
+				permutation(i, cnt+1);
+				visit[i] = false;
+			}
+		}
+	}
+}
+/*
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _BaekJoon15649 {
+	static int N, M, arr[], select[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,10 +69,13 @@ public class _BaekJoon15649 {
 		}
 		for(int i=0; i<N; i++) {
 			select[cnt] = arr[index];
-		permutation(index+1, cnt+1);
-		permutation(index+1, cnt);
+			permutation(index+1, cnt+1);
+			permutation(index+1, cnt);
 		}
 		
 		
 	}
 }
+
+
+*/
