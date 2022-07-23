f078665dee6dfba6512b8f68565136fb6d3f83ca
BaekJeongEun <sophia991106@gmail.com>
2022-07-23 20:29:21 +0900

    [BaekJoon]: 랜선 자르기 (S2)

--- CodingTest/src/BaekJoon/BaekJoon1654.java
+++ CodingTest/src/BaekJoon/BaekJoon1654.java
@@ -6,32 +6,38 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class _BaekJoon1654 {
-	static int n, k, arr[];
+public class BaekJoon1654 {
+	static long k, arr[];
+	static int n;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		arr = new int[n];
+		arr = new long[n];
 		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(arr);
-		System.out.println(rec(0,arr[arr.length-1], arr[arr.length-1]/2, 0));
+		System.out.println(rec(1,arr[arr.length-1], 0));
 	}
-	private static int rec(int s, int e, int mid, int sum) {
-		
-		int cur_mid = (s+e)/2;
-		if(s>e) return cur_mid;
+	private static int rec(long s, long e, int result) {
+		//System.out.println(s+" "+e);
+		if(s>e) return result;
+		long cur_mid =  (long) Math.floor((s+e))/2;
 		int temp_sum=0;
 		for(int i=0; i<n; i++) {
-			temp_sum += arr[i]/cur_mid;
+			temp_sum += (arr[i]/cur_mid);
 		}
+			
 		if(temp_sum<k) { // 개수가 적다 -> 길이를 줄여야 한다
-			return rec(s, mid-1, cur_mid, sum);
+			e = cur_mid - 1;
 		}else {
-			return rec(mid+1, e, cur_mid, sum); 
-		}
+			s = cur_mid + 1;
+			result = (int)cur_mid;
+		} 
+		int next_result = rec(s, e, result);
+		
+		return Math.max(result, next_result);
 	}
 }
