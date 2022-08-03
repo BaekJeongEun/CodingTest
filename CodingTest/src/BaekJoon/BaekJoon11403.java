04731b135f3271cc640aad9eaf433247bd48972d
BaekJeongEun <sophia991106@gmail.com>
2022-08-03 20:42:10 +0900

    [BaekJoon]: 경로 찾기

---
+++ CodingTest/src/BaekJoon/BaekJoon11403.java
@@ -0,0 +1,57 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon11403 { // 경로 찾기
+	static int N;
+	static int[][] arr;
+	static int[][] result;
+	static boolean[][] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		result = new int[N][N];
+		visit = new boolean[N][N];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		result = Arrays.copyOf(arr, arr.length);
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(arr[i][j] == 1) {
+					result[i][j]=1;
+					dfs(i, j);
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				sb.append(result[i][j]).append(" ");
+			}sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static void dfs(int start, int idx) {
+		for(int i=0; i<N; i++) {
+			if(arr[idx][i]==1 && result[start][i]==0) {
+				result[start][i] = 1;
+				dfs(start, i);
+			}
+		}
+	}
+
+}
