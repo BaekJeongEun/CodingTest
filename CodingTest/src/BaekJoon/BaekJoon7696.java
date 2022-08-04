0360f3b6d3f0916fce58dbc2ee4ee61119ef5c17
BaekJeongEun <sophia991106@gmail.com>
2022-08-05 01:34:08 +0900

    [BaekJoon]: 반복하지 않는 수 (S4)

--- CodingTest/src/BaekJoon/BaekJoon7696.java
+++ CodingTest/src/BaekJoon/BaekJoon7696.java
@@ -5,40 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class BaekJoon7696 {
-
+public class BaekJoon7696 { // 반복하지 않는 수 (S4)
+	static int[] arr = new int[1000001];
+	static int[] count = new int[10];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-		int cnt=0, i=1, index=-1, num=0;
-		boolean exit = false;
-		String str = "";
-		int[] arr = new int[10];
+		int cnt=0, i=1, index=-1;
+		
+		arr[1] = 1;
+		int num = 2;
+		for(int i1=2; i1<=1000000; i1++) { // 배열 채우기
+			// 지금 숫자가 중복되는 수인지
+			num += dupl(num); // 중복되는 갯수 반환
+			arr[i1] = num++;
+		}
+		
+		
 		while (N != 0) { // 수 입력 받기
-			cnt = 0;
-			for (i = 1; cnt != N; i++) { // 카운트 셀 거임, 카운트가 타겟 숫자 N이 될 때까지 반복
-				Arrays.fill(arr, 0);
-				exit = false;
-				num = i;
-				while(num != 0) { // 현재 수에 중복 숫자가 있는지
-					index = num%10;
-					arr[index]++;
-					//System.out.println(num+"  "+index);
-					num /= 10;
-					if (arr[index] > 1) { // 중복 있다면 당장 멈춰
-						exit = true;
-						break;
-					}
-				}
-				if (!exit) { // 중복 없었다면 카운트해줌, 중복 있었으면 카운트 안 쳐줌! 다시 돌아
-					cnt++;
-				}
-			}
-			System.out.println(i-1);
+			System.out.println(arr[N]);
 
 			N = Integer.parseInt(br.readLine());
 		}
+	}
+	
+	private static int dupl(int num) {
+		boolean exit = false;
+		int cnt = 0;
+		int temp = num;
+		while(!exit) {
+			exit = true;
+			Arrays.fill(count, 0);
+			while(temp != 0) { // 현재 수에 중복 숫자가 있는지
+				count[temp%10]++;
+
+				if (count[temp%10] > 1) { // 중복 있다면 당장 멈춰
+					num++;
+					temp = num;
+					exit = false;
+					cnt++;
+					break;
+				}
+				temp /= 10;
+			}
+			
+		} 
+		
+		return cnt;
 	}
 
 }
