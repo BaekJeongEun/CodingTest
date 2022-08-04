93e7acb3b5d436c70f1d63ece831f7a8cb22165d
BaekJeongEun <sophia991106@gmail.com>
2022-08-04 23:45:49 +0900

    [BaekJoon]: 조합 0의 개수 (S2)

---
+++ CodingTest/src/BaekJoon/BaekJoon2004.java
@@ -0,0 +1,33 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon2004 { // 조합 0의 개수 (S2)
+	static int[] arr;
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		long n_m = n-m;
+		
+		long a = get(5, n) - get(5, n_m) - get(5, m);
+		long b = get(2, n) - get(2, n_m) - get(2, m);
+		
+		System.out.println(Math.min(a, b));
+
+	}
+	
+	private static long get(long loop, long n) {
+		
+		long cnt=0;
+		while(n >= loop) {
+			cnt += n/loop;
+			n /= loop;
+		}
+		
+		return cnt;
+	}
+	
+
+}
