14fce5ff041aaf2fd03f28d64edfda8177ca84b7
BaekJeongEun <sophia991106@gmail.com>
2022-08-08 21:30:15 +0900

    [BaekJoon]: 벽 부수고 이동하기 (G4)

--- CodingTest/src/BaekJoon/BaekJoon2206.java
+++ CodingTest/src/BaekJoon/BaekJoon2206.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BaekJoon2206 {
+public class BaekJoon2206 { // 벽 부수고 이동하기 (G4)
 	static int N, M, arr[][], cnt = Integer.MAX_VALUE;
 	static int dx[] = { 0, 0, 1, -1 };
 	static int dy[] = { -1, 1, 0, 0 };
@@ -37,14 +37,14 @@ public class BaekJoon2206 {
 	private static int bfs() {
 		boolean[][][] visited = new boolean[2][N][M];
 		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(0, 0, 1, 0));
+		q.add(new Point(0, 0, 1, false));
 		visited[0][0][0] = true;
 		while (!q.isEmpty()) {
 
 			Point now = q.poll();
 			int nowX = now.x;
 			int nowY = now.y;
-			int wall = now.wall;
+			boolean wall = now.wall;
 			int cnt = now.cnt;
 			if (nowX == N - 1 && nowY == M - 1) {
 				return now.cnt;
@@ -53,15 +53,20 @@ public class BaekJoon2206 {
 				int newX = nowX + dx[i];
 				int newY = nowY + dy[i];
 				
-				if (newX < 0 || newY < 0 || newX >= N || newY >= M || visited[wall][newX][newY]) {
+				if (newX < 0 || newY < 0 || newX >= N || newY >= M) {
 					continue;
 				}
 				if(arr[newX][newY] == 0) {
-					visited[wall][newX][newY] = true;
-                    q.add(new Point(newX, newY, cnt + 1, wall));
-				}else if(arr[newX][newY] == 1 && wall == 0) {	// 벽(1)이지만 부순 적 없기 때문에 한 번은 봐준다~!
-					visited[wall][newX][newY] = true;
-					q.add(new Point(newX, newY, cnt+1, wall+1));
+					if(!now.wall && !visited[0][newX][newY]) {
+						visited[0][newX][newY] = true;
+	                    q.add(new Point(newX, newY, cnt + 1, false));
+					}else if(now.wall && !visited[1][newX][newY]) {
+						visited[1][newX][newY] = true;
+	                    q.add(new Point(newX, newY, cnt + 1, true));
+					}
+				}else if(!wall) {	// 벽(1)이지만 부순 적 없기 때문에 한 번은 봐준다~!
+					visited[1][newX][newY] = true;
+					q.add(new Point(newX, newY, cnt+1, true));
 				}
 			}
 
@@ -71,9 +76,9 @@ public class BaekJoon2206 {
 
 	static class Point {
 		int x, y, cnt;
-		int wall;
+		boolean wall;
 
-		public Point(int x, int y, int cnt, int wall) {
+		public Point(int x, int y, int cnt, boolean wall) {
 			this.x = x;
 			this.y = y;
 			this.wall = wall; // 벽 부순 횟수
