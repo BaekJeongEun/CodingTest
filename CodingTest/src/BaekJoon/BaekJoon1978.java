91fa459cd2bde35b67dfb63fcb51d899f76fdb27
BaekJeongEun <sophia991106@gmail.com>
2022-08-09 22:25:01 +0900

    [BaekJoon]: 소수 찾기 (S5)

--- CodingTest/src/BaekJoon/BaekJoon1978.java
+++ CodingTest/src/BaekJoon/BaekJoon1978.java
@@ -5,25 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BaekJoon1978 {
-
+public class BaekJoon1978 { // 소수 찾기 (S5)
+	static int[] arr;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine()), cnt=0;
+		
+		arr = new int[1001];
+		arr[1] = 1;
+		for(int i=2; i<=1000; i++) {
+			sosu(i);
+		}
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		while(st.hasMoreTokens()) {
-			if(sosu(Integer.parseInt(st.nextToken())))
-					cnt++;
+			if(arr[Integer.parseInt(st.nextToken())] ==0)
+				cnt++;
 		}
 		System.out.println(cnt);
 	}
-	static boolean sosu(int num) {
+	static void sosu(int num) {
 		int cnt=0;
-		for(int i=1; i<=num; i++) {
+		for(int i=2; i*i<=num; i++) {
 			if(num%i==0)
 				cnt++;
 		}
-		return (cnt==2)?true:false;
+		arr[num] = (cnt==0)?0:cnt;
 	}
 
 }
