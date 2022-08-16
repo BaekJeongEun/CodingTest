ac16867a7006f0f3296fb44cca457e27e97f3776
BaekJeongEun <sophia991106@gmail.com>
2022-08-16 17:36:09 +0900

    [Jungol]: 냉장고

--- CodingTest/src/SWExpertAcademy/Jungol1828.java
+++ CodingTest/src/SWExpertAcademy/Jungol1828.java
@@ -22,26 +22,18 @@ public class Jungol1828 {
 			arr[k][1] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr, new Comparator<int[]>() {
-
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				return o1[0] - o2[0];
+				return o1[1] - o2[1];
 			}
 
 		});
 		
-		
 		int pre = arr[0][1];
-		for(int i=1; i<N; i++) {//System.out.println(arr[i][0]+" "+arr[i][1] +" pre "+pre +" i "+i);
-			if(arr[i][0] <= pre) {
-				continue;
-			}
-			else
-			{
+		for(int i=1; i<N; i++) {
+			if(pre<arr[i][0]) {
 				pre = arr[i][1];
 				cnt++;
-				//System.out.println("cnt "+cnt);
-				//System.out.println("pre "+pre);
 			}
 		}
 		System.out.println(cnt);
