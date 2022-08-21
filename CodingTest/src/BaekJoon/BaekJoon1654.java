546b066b533d91786dec3362ec5d91c7d2e037ee
BaekJeongEun <sophia991106@gmail.com>
2022-08-21 17:36:35 +0900

    [BaekJoon]: 랜선 자르기 (S2)

--- CodingTest/src/BaekJoon/BaekJoon1654.java
+++ CodingTest/src/BaekJoon/BaekJoon1654.java
@@ -3,41 +3,32 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BaekJoon1654 {
-	static long k, arr[];
-	static int n;
-	public static void main(String[] args) throws IOException{
+public class BaekJoon1654 { // 랜선 자르기 (S2)
+
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		arr = new long[n];
-		for(int i=0; i<n; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		long K = Integer.parseInt(st.nextToken());
+		long mid =0, start=1, end=Long.MIN_VALUE, total=0;
+		long[] arr = new long[N];
+		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			end = Math.max(end, arr[i]);
 		}
-		Arrays.sort(arr);
-		System.out.println(rec(1,arr[arr.length-1], 0));
-	}
-	private static int rec(long s, long e, int result) {
-		//System.out.println(s+" "+e);
-		if(s>e) return result;
-		long cur_mid =  (long) Math.floor((s+e))/2;
-		int temp_sum=0;
-		for(int i=0; i<n; i++) {
-			temp_sum += (arr[i]/cur_mid);
+		while(start<=end) {
+			mid = (start+end)/2;
+			total = 0;
+			for(int i=0; i<N; i++) {
+				total += arr[i]/mid;
+			}
+			if(total < K) { // 만들어야 할 랜선 개수보다 적다면 길이 줄여서 개수 늘리기
+				end = mid - 1;
+			}else start = mid + 1;
 		}
-			
-		if(temp_sum<k) { // 개수가 적다 -> 길이를 줄여야 한다
-			e = cur_mid - 1;
-		}else {
-			s = cur_mid + 1;
-			result = (int)cur_mid;
-		} 
-		int next_result = rec(s, e, result);
-		
-		return Math.max(result, next_result);
+		System.out.println(end);
 	}
 }
