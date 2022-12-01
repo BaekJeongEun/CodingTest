c431887d99488e53604fb5871027970a24b767b7
BaekJeongEun <sophia991106@gmail.com>
2022-12-01 10:39:31 +0900

    [BaekJoon]: 테트로미노 (G4)

---
+++ CodingTest/src/BaekJoon/BaekJoon14500.java
@@ -0,0 +1,57 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BaekJoon14500 { // 테트로미노 (G4)
+	static int N, M, max = Integer.MIN_VALUE;
+	static int[][] arr, dir= {{1,0},{-1,0},{0,1},{0,-1}};
+	static boolean[][] visit;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		visit = new boolean[N][M];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				visit[i][j] = true;
+				dfs(i, j, 1, arr[i][j]);
+				visit[i][j] = false;
+			}
+		}
+		System.out.println(max);
+	}
+	private static void dfs(int x, int y, int cnt, int sum) {
+		if(cnt == 4) {
+			max = Math.max(max, sum);
+			return;
+		}
+		for(int d=0; d<4; d++) {
+			int nx = x + dir[d][0];
+			int ny = y + dir[d][1];
+			if(nx<0 || ny<0 || nx>=N || ny>=M || visit[nx][ny]) continue;
+			if(cnt == 2) {
+				visit[nx][ny] = true;
+				dfs(x, y, cnt+1, sum+arr[nx][ny]);
+				visit[nx][ny] = false;
+			}
+			visit[nx][ny] = true;
+			dfs(nx, ny, cnt+1, sum+arr[nx][ny]);
+			visit[nx][ny] = false;
+		}
+	}
+
+}
