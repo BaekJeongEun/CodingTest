842537b8f166a65b0ead223b2458264d8c2ba71e
BaekJeongEun <sophia991106@gmail.com>
2022-12-17 22:01:59 +0900

    [BaekJoon]: hide (G5)

--- CodingTest/src/BaekJoon/BaekJoon13549.java
+++ CodingTest/src/BaekJoon/BaekJoon13549.java
@@ -1,45 +1,50 @@
 package BaekJoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class BaekJoon13549 { // 숨바꼭질 3(G5)
 	static int N, K, min = Integer.MAX_VALUE;
-	static int[][] dir = {{0,-1},{0,1},{-1,0},{1,0}};
+	static int[][] dir = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+	static boolean[] visited = new boolean[100001];
+
 	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		bfs();
-	}
-	private static void bfs() {
-		Queue<int[]> q = new ArrayDeque<int[]>();
-		q.add(new int[] {N, 0});
-		while(!q.isEmpty()) {
-			for(int d=0; d<4; d++) {
-				int[] now = q.poll();
-				int cur = now[0];
-				int time = now[1];
-				if(cur == K) {
-					System.out.println(time);
-					System.exit(0);
-				}
-				// -1 걷기
-				q.offer(new int[] {cur-1, time+1});
-				// 1 걷기
-				q.offer(new int[] {cur+1, time+1});
-				// 순간이동
-				q.offer(new int[] {cur*2, time});
+		boolean[] visited = new boolean[100001];
+		LinkedList<int[]> queue = new LinkedList<int[]>();
+		queue.add(new int[] {N, 0});
+		
+		int X, time = 0;
+		while (!queue.isEmpty()) {
+			int[] tmp = queue.poll();
+			X = tmp[0];
+			time = tmp[1];
+			visited[X] = true;
+			
+			// 수빈이가 동생을 찾음
+			if (X == K) break;
+			
+			// 걷거나 순간이동
+			if (X*2 <= 100000 && !visited[X*2]) {
+				queue.addFirst(new int[] {X*2, time});
+			}
+			if (X < K && X < 100000 && !visited[X+1]) {
+				queue.add(new int[] {X+1, time+1});
+			}
+			if (X > 0 && !visited[X-1]) {
+				queue.add(new int[] {X-1, time+1});
 			}
 		}
+		System.out.println(time);
+
 	}
 
 }
