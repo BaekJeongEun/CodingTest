48b568043ad4968a50645abcd9d9a0afaa502cb8
BaekJeongEun <sophia2404@naver.com>
2023-03-01 22:02:11 +0900

    [Programmers]: 최댓값과 최솟값 (L2)

--- CodingTest/src/Programmers/MaxMin.java
+++ CodingTest/src/Programmers/MaxMin.java
@@ -1,19 +1,20 @@
 package Programmers;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MaxMin { // 최댓값과 최솟값 (L2)
 
 	public static void main(String[] args) {
-		System.out.println(solution("-1 -1"));
+		System.out.println(solution("10 -1"));
 	}
 	public static String solution(String s) {
         String[] arr = s.split(" ");
         List<Integer> list =Arrays.stream(arr)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        return list.stream().mapToInt(x->x).min().getAsInt() + " " + list.stream().mapToInt(x->x).max().getAsInt();
-    }
+        return Collections.min(list) + " " + Collections.max(list);
+	}
 }
