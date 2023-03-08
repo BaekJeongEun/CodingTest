9d69d28f423b438739765367eaa6c395ca72fae5
BaekJeongEun <sophia2404@naver.com>
2023-03-08 16:48:07 +0900

    [BaekJoon]: 최대 힙 (S2)

---
+++ CodingTest/src/BaekJoon/BaekJoon11279.java
@@ -0,0 +1,36 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BaekJoon11279 { // 최대 힙 (S2)
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		PriorityQueue<Integer> arr = new PriorityQueue(Comparator.reverseOrder());
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 0) {
+				if(!arr.isEmpty()) {
+					sb.append(arr.poll()).append("\n");
+				} else {
+					sb.append("0").append("\n");
+				}
+			} else {
+				arr.offer(num);
+			}
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
