7833195d96267054fcaa6244971adf762eb0d526
BaekJeongEun <sophia2404@naver.com>
2023-07-13 00:14:39 +0900

    [SWExpertAcademy]: 부자의 꿈 (개선)

--- CodingTest/src/SWExpertAcademy/SWExpertAcademy_Rich.java
+++ CodingTest/src/SWExpertAcademy/SWExpertAcademy_Rich.java
@@ -1,73 +1,111 @@
 package SWExpertAcademy;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class SWExpertAcademy_Rich {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int TC = Integer.parseInt(br.readLine());
-		int N,M,Q;
-		for(int tc=1; tc<=TC; tc++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			Q = Integer.parseInt(st.nextToken());
+   private static int TC, N, M, Q, result;
+   private static int[][] cells;
+   private static boolean[][] visit;
 
-			PriorityQueue<Integer> row[] = new PriorityQueue[N+1];
-			PriorityQueue<Integer> col[] = new PriorityQueue[M+1];
-			int[][] arr = new int[N+1][M+1];
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st;
+      TC = Integer.parseInt(br.readLine());
 
-			for(int i=1; i<=N; i++) {
-				row[i] = new PriorityQueue<>();
-			}
-			for(int i=1; i<=M; i++) {
-				col[i] = new PriorityQueue<>();
-			}
-			
-			for(int i=1; i<=N; i++) {
-				st = new StringTokenizer(br.readLine());
-				int num = Integer.parseInt(st.nextToken());
-				for(int j=1; j<=M; j++) {
-					row[i].offer(num);
-					col[j].offer(num);
-					arr[i][j] = num;
-				}
-			}
-			int sum = 0;
-			for(int q=0; q<Q; q++) {
-				st = new StringTokenizer(br.readLine());
-				int r = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-				int num = Integer.parseInt(st.nextToken());
-				row[r].offer(num);
-				col[c].offer(num);
-				arr[r][c] = num;
-				
-				for(int i=1; i<=N; i++) {
-					for(int j=1; j<=M; j++) {
-						int now = arr[i][j];
-						if(row[i].peek() < now && col[i].peek() < now) {
-							sum++;
-						}
-					}
-				}
-			}
-			sb.append("#"+tc+" ").append(sum).append("\n");
+      for (int t = 1; t <= TC; t++) {
+         st = new StringTokenizer(br.readLine());
+         N = Integer.parseInt(st.nextToken());
+         M = Integer.parseInt(st.nextToken());
+         Q = Integer.parseInt(st.nextToken());
+         cells = new int[N][M];
+         visit = new boolean[N][M];
+         result = 0;
+         for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+            for (int m = 0; m < M; m++) {
+               cells[n][m] = Integer.parseInt(st.nextToken());
+            }
+         }
+
+         for (int q = 0; q < Q; q++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken()) - 1;
+            int m = Integer.parseInt(st.nextToken()) - 1;
+            cells[n][m] = Integer.parseInt(st.nextToken());
+            updateArray(n, m);
+            PriorityQueue<int[]> pQueue = createQueue();
+
+            while (!pQueue.isEmpty()) {
+            	int[] current = pQueue.poll();
+               result += check(current);
+            }
+         }
+         System.out.println("#" + t + " " + result);
+      }
+   }
+
+   private static int check(int[] mold) {
+      int cn = mold[0];
+      int cm = mold[1];
+      int num = mold[2];
+
+      for (int n = 0; n < N; n++) {
+         if (cells[n][cm] > num) {
+            return 0;
+         } else {
+            visit[n][cm] = true;
+         }
+      }
+      for (int m = 0; m < M; m++) {
+         if (cells[cn][m] > num) {
+            return 0;
+         } else {
+            visit[cn][m] = true;
+         }
+      }
+      visit[cn][cm] = false;
+      return 1;
+   }
+
+   private static void updateArray(int cn, int cm) {
+      for (int n = 0; n < N; n++) {
+         if (cells[cn][cm] < cells[n][cm])
+            visit[n][cm] = false;
+      }
+      for (int m = 0; m < M; m++) {
+         if (cells[cn][cm] < cells[cn][m])
+            visit[cn][m] = false;
+      }
+      visit[cn][cm] = false;
+   }
+
+   private static PriorityQueue<int[]> createQueue() {
+      PriorityQueue<int[]> pQueue = new PriorityQueue<>(new Comparator<int[]>() {
+
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			return o1[2] - o2[2];
 		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+	});
+      int maxN, maxM, maxNum;
 
+      for (int n = 0; n < N; n++) {
+         maxN = maxM = 0;
+         maxNum = Integer.MIN_VALUE;
+         for (int m = 0; m < M; m++) {
+            if (!visit[n][m] && cells[n][m] > maxNum) {
+               maxNum = cells[n][m];
+               maxN = n;
+               maxM = m;
+            }
+         }
+         pQueue.offer(new int[] {maxN, maxM, maxNum});
+      }
+      return pQueue;
+   }
 }
