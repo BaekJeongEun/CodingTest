e8ce6aba61d859b4b3209c8ea460258c86d8b45d
BaekJeongEun <sophia2404@naver.com>
2023-07-14 23:37:59 +0900

    [BaekJoon]: 돌 게임 (S5)

--- CodingTest/src/BaekJoon/BaekJoon9655.java
+++ CodingTest/src/BaekJoon/BaekJoon9655.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BaekJoon9655 { // 돌 게임 (S5)
+public class BaekJoon9655 {
 	static int N;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
