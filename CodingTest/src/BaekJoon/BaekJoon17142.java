68b6643cc2eebd49d160e428026568a2996c51ee
BaekJeongEun <sophia2404@naver.com>
2023-08-05 22:05:38 +0900

    [BaekJoon]: 연구소 3(G3)

--- CodingTest/src/BaekJoon/BaekJoon17142.java
+++ CodingTest/src/BaekJoon/BaekJoon17142.java
@@ -3,139 +3,110 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BaekJoon17142 { // 낚시왕 (G1)
-    static int R, C, M, total;
-    static Point[][] arr;
-    static ArrayList<Point> list = new ArrayList();
-    static int[][] dir = { {}, { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // 상1, 하2, 우3, 좌4
+public class BaekJoon17142 { // 연구소 3(G3)
+	static int N, M, min = Integer.MAX_VALUE, total;
+	static ArrayList<Point> virus;
+	static ArrayList<Point> pick;
+	static Queue<Point> q;
+	static int[][] arr, dir = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+	static boolean[][] visit;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new Point[R + 1][C + 1];
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken()); // 속력
-            int d = Integer.parseInt(st.nextToken()); // 이동방향
-            int z = Integer.parseInt(st.nextToken()); // 크기
-            Point now = new Point(r, c, s, d, z);
-            list.add(now);
-            arr[r][c] = now;
-        }
-        for (int i = 1; i <= C; i++) {
-            total += fishing(i);
-            move();
-        }
-        System.out.println(total);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][N];
+		virus = new ArrayList<>();
+		pick = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				if (num == 2) {
+					virus.add(new Point(i, j, 0));
+					arr[i][j] = 2;
+				} else {
+					arr[i][j] = num;
+					if (num == 0)
+						total++;
+				}
+			}
+		}
+		if (total == 0) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		comb(0, 0);
+		System.out.println((min == Integer.MAX_VALUE) ? -1 : min);
+	}
 
-    /**
-     * 낚시왕이 있는 열에 있는 상어 중에서 땅과 제일 가까운 상어를 잡는다. 상어를 잡으면 격자판에서 잡은 상어가 사라진다.
-     */
-    private static int fishing(int king) {
-        int size = 0;
-        for (int r = 1; r <= R; r++) { 
-            if (arr[r][king] != null) {
-                size = arr[r][king].z;
-                list.remove(arr[r][king]);
-                arr[r][king] = null;
-                break;
-            }
-        }
-        return size;
-    }
+	private static void comb(int cnt, int idx) {
+		if (cnt == M) {
+			// 바이러스 놓기
+			q = new ArrayDeque();
+			visit = new boolean[N][N];
+			for (int i = 0; i < pick.size(); i++) {
+				Point now = pick.get(i);
+				q.offer(now);
+				visit[now.x][now.y] = true;
+			}
+			bfs(total, visit);
+			return;
+		}
+		for (int i = idx; i < virus.size(); i++) {
+			pick.add(virus.get(i));
+			comb(cnt + 1, i + 1);
+			pick.remove(virus.get(i));
+		}
+	}
 
-    /**
-     * 상어가 이동한다.
-     */
-    private static void move() {
-        int size = list.size();
-        PriorityQueue<Point> q = new PriorityQueue<>(new Comparator<Point>() {
-            @Override
-            public int compare(Point o1, Point o2) {
-                return o2.z - o1.z;
-            }
-        });
-        for (int qs = 0; qs < size; qs++) {
-            Point now = list.get(0);
-            int s = now.s;
-            int d = now.d;
-            int z = now.z;
-            int r = now.r, c = now.c;
-            int dist = 0;
-            if (d <= 2) { // 상, 하 일 경우 c 고정 S%(R*2-2)
-                dist = s % (R * 2 - 2);
-                while (dist-- > 0) {
-                    if (d == 1) { // 상
-                        r--;
-                    } else { // 하
-                        r++;
-                    }
-                    if (r == 0) { // 상방향으로 갔다가 범위 벗어나면
-                        d = 2; // 아래 방향으로 바꿔
-                        r = 2; // 위치도 +1
-                    } else if (r == R+1) { // 하
-                        d = 1; 
-                        r = R-1;
-                    }
-                }
-            } else { // 좌, 우 일 경우 r 고정
-                dist = s % (C * 2 - 2);
-                while (dist-- > 0) {
-                    if (d == 3) { // 우
-                        c++;
-                    } else { // 좌
-                        c--;
-                    }
-                    if (c == C+1) { // 우
-                        d = 4;
-                        c = C-1;
-                    } else if (c == 0) { // 좌
-                        d = 3;
-                        c = 2;
-                    }
-                }
-            }
-            list.remove(0);
-            
-            q.offer(new Point(r, c, s, d, z));
-        }
-        int sum = 0;
-        arr = new Point[R + 1][C + 1];
-        while(!q.isEmpty()) {
-            Point now = q.poll();
-            if(arr[now.r][now.c] == null) {
-                arr[now.r][now.c] = now;
-                list.add(now);
-            }
-        }
-    }
+	private static void bfs(int origin, boolean[][] visit) {
+		while (!q.isEmpty()) {
+			Point now = q.poll();
+			int x = now.x;
+			int y = now.y;
+			int time = now.time;
+			for (int d = 0; d < 4; d++) {
+				int nx = x + dir[d][0];
+				int ny = y + dir[d][1];
 
-    private static class Point {
-        int r, c, s, d, z;
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N || visit[nx][ny] || arr[nx][ny] == 1)
+					continue;
 
-        public Point(int r, int c, int s, int d, int z) {
-            super();
-            this.r = r;
-            this.c = c;
-            this.s = s;
-            this.d = d;
-            this.z = z;
-        }
+				if (arr[nx][ny] == 0)
+					origin--;
 
-        @Override
-        public String toString() {
-            return "Point [r=" + r + ", c=" + c + ", s=" + s + ", d=" + d + ", z=" + z + "]";
-        }
-    }
+				if (origin == 0) {
+					min = Math.min(min, time + 1);
+					return;
+				}
+
+				q.offer(new Point(nx, ny, time + 1));
+				visit[nx][ny] = true;
+			}
+
+		}
+	}
+
+	static class Point {
+		int x, y, time;
+
+		public Point(int x, int y, int time) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
+
+		@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + "]";
+		}
+	}
 }
