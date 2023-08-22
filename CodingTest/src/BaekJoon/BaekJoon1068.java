00d72a3b957663a4e4f51163287b7593144a02de
BaekJeongEun <sophia2404@naver.com>
2023-08-22 19:59:15 +0900

    [BaekJoon]: 트리(G5)

--- CodingTest/src/BaekJoon/BaekJoon1068.java
+++ CodingTest/src/BaekJoon/BaekJoon1068.java
@@ -9,32 +9,34 @@ import java.util.StringTokenizer;
 public class BaekJoon1068 { // 트리(G5)
 	static int answer, remove;
 	static Node[] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		arr = new Node[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			arr[i] = new Node(i);
 		}
 		int root = 0;
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			int parent = Integer.parseInt(st.nextToken());
-			if(parent == -1) {
+			if (parent == -1) {
 				root = i;
 				continue;
 			}
 			arr[parent].childs.add(arr[i]);
 		}
 		remove = Integer.parseInt(br.readLine());
-		if(remove != root) {
+		if (remove != root) {
 			deleteNode(root);
 			dfs(root);
 		}
 	}
+
 	static void deleteNode(int idx) {
-		for(Node child : arr[idx].childs) {
-			if(child.idx == remove) {
+		for (Node child : arr[idx].childs) {
+			if (child.idx == remove) {
 				arr[idx].childs.remove(child);
 				return;
 			}
@@ -47,14 +49,16 @@ public class BaekJoon1068 { // 트리(G5)
 			answer++;
 			return;
 		}
-		
-		for(Node child : arr[idx].childs) {
+
+		for (Node child : arr[idx].childs) {
 			dfs(child.idx);
 		}
 	}
+
 	static class Node {
 		int idx;
 		ArrayList<Node> childs = new ArrayList<>();
+
 		public Node(int idx) {
 			this.idx = idx;
 		}
