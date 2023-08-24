7d7858423cdd4711f93463704958742920a91411
BaekJeongEun <sophia2404@naver.com>
2023-08-24 16:15:16 +0900

    [BaekJoon]: 회장뽑기(G5)

---
+++ CodingTest/src/BaekJoon/BaekJoon2660.java
@@ -0,0 +1,88 @@
+package BaekJoon;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BaekJoon2660 { // 회장뽑기(G5)
+	static final int INF = 987654321;
+	 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+ 
+        int N = Integer.parseInt(br.readLine());
+ 
+        int[][] arr = new int[N + 1][N + 1];
+ 
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) {
+                    arr[i][j] = INF;
+                }
+            }
+        }
+ 
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+ 
+            if (x == -1 && y == -1) {
+                break;
+            }
+ 
+            arr[x][y] = arr[y][x] = 1; 
+        }
+ 
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+ 
+        int readerScore = INF;
+ 
+        int[] scoreArr = new int[N + 1]; // 친구 점수 목록
+ 
+        for (int i = 1; i <= N; i++) {
+            int score = 0;
+            for (int j = 1; j <= N; j++) {
+                if (arr[i][j] != INF) {
+                    score = Math.max(score, arr[i][j]);
+                }
+            }
+ 
+            scoreArr[i] = score;
+            readerScore = Math.min(readerScore, score);
+        }
+ 
+        StringBuilder title = new StringBuilder();
+        title.append(readerScore + " ");
+ 
+        int readerNum = 0;
+ 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (readerScore == scoreArr[i]) {
+                readerNum++;
+                sb.append(i + " ");
+            }
+        }
+ 
+        title.append(readerNum + "\n");
+ 
+        bw.write(title.toString());
+        bw.write(sb.toString() + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+}
