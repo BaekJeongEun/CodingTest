d591539007df1cd297361c21ff17aab808232497
BaekJeongEun <sophia2404@naver.com>
2023-09-01 22:54:28 +0900

    [BaekJoon]: 알고리즘 수업 - 너비 우선 탐색 1(S2)

--- CodingTest/src/BaekJoon/BaekJoon24444.java
+++ CodingTest/src/BaekJoon/BaekJoon24444.java
@@ -6,66 +6,66 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BaekJoon24444 { // 알고리즘 수업 - 너비 우선 탐색 1(S2)
-	static int N, M, R;
-	static boolean visited[];
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static int N, M;
+    static List<List<Integer>> l = new ArrayList<>();
+    static int[] visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-		visited = new boolean[N + 1];
-		for (int i = 0; i < N; i++) {
-			graph.add(new ArrayList<>());
-		}
+        visited = new int[N+1];
+        for(int i = 0; i<=N; i++)
+            l.add(new ArrayList<>());
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
+        for(int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
 
-			graph.get(start).add(end);
-			graph.get(end).add(start);
-		}
-		for (int i = 1; i < N; i++) {
-			Collections.sort(graph.get(i));
-		}
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
-		BVFS(R);
-	}
+            l.get(A).add(B);
+            l.get(B).add(A);
+        }
 
-	private static void BVFS(int r) {
-		Queue<Integer> queue = new LinkedList<>();
-		int count = 0;
-		int result[] = new int[N + 1];
-		queue.add(r);
-		visited[r] = true;
-		result[r] = ++count;
+        for(int i = 1; i<=N; i++)
+            Collections.sort(l.get(i));
 
-		while (!queue.isEmpty()) {
-			int now = queue.poll();
+        bfs(R);
 
-			for (int i = 0; i < graph.get(now).size(); i++) {
-				int next = graph.get(now).get(i);
-				if (!visited[next]) {
-					queue.add(next);
-					visited[next] = true;
-					result[next] = ++count;
-				}
-			}
+        for(int i = 1; i<=N; i++)
+            System.out.println(visited[i]);
+    }
 
-		}
-		for (int i = 1; i < result.length; i++)
-			System.out.println(result[i]);
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        int cnt = 1;
 
-	}
+        q.offer(start);
+        visited[start] = cnt++;
+
+        while(!q.isEmpty()){
+            int a = q.poll();
+
+            for(int i = 0; i<l.get(a).size(); i++){
+                int nextV = l.get(a).get(i);
+
+                if(visited[nextV] != 0)
+                    continue;
+
+                q.offer(nextV);
+                visited[nextV] = cnt++;
+            }
+        }
+    }
 
 }
