15c2f403a0d2aa30fc65e20c509dbd5a1ed51ea2
BaekJeongEun <sophia2404@naver.com>
2023-09-07 15:31:19 +0900

    [BaekJoon]: 녹색거탑(B4)

--- CodingTest/src/BaekJoon/BaekJoon24267.java
+++ CodingTest/src/BaekJoon/BaekJoon24267.java
@@ -3,19 +3,16 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class BaekJoon24267 { // 알고리즘의 수행 시간 5
+public class BaekJoon24267 { // 녹색거탑(B4)
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-        long n = Long.parseLong(br.readLine());
-
-        System.out.println(n * n * n);
-        System.out.println(3);
-        br.close();
+        int n = Integer.parseInt(br.readLine());
+        int calc = 1;
+        while (n-->0) calc*=2;
+      
+        System.out.println(calc);
 
 	}
 
