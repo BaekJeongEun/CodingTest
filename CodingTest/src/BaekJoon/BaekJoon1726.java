3eedb4d3c1ea28265fcbecc1fdaca1c58d7cdaf1
BaekJeongEun <sophia2404@naver.com>
2023-09-09 20:30:59 +0900

    [BaekJoon]: 로봇 (G3)

--- CodingTest/src/BaekJoon/BaekJoon1726.java
+++ CodingTest/src/BaekJoon/BaekJoon1726.java
@@ -3,7 +3,7 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
