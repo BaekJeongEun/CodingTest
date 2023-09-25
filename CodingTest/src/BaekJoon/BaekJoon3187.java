5169fd2ebc371808c8b33485d5047a95a161b150
BaekJeongEun <sophia2404@naver.com>
2023-09-25 16:03:37 +0900

    [BaekJoon]: 양치기 꿍(S1)

--- CodingTest/src/BaekJoon/BaekJoon3187.java
+++ CodingTest/src/BaekJoon/BaekJoon3187.java
@@ -1,80 +1,74 @@
 package BaekJoon;
 
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BaekJoon3187 { // 양치기 꿍(S1)
-	private static boolean[][] isVisited;
-    private static char[][] map;
-    private static int R;
-    private static int C;
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, -1, 0, 1};
-    private static int sheepResult = 0;
-    private static int wolfResult = 0;
+	static int N, M, wolf, sheep;
+	static char[][] arr;
+	static boolean[][] visit;
+	static int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        R = sc.nextInt(); //행
-        C = sc.nextInt(); //열
-        map = new char[R][C];
-        isVisited = new boolean[R][C];
-        for (int i = 0; i < R; i++) {
-            String line = sc.next();
-            for (int j = 0; j < C; j++) {
-                map[i][j] = line.charAt(j);
-            }
-        }
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (!isVisited[i][j] && map[i][j] != '#') {
-                    bfs(new Point(i, j));
-                }
-            }
-        }
-        System.out.println(sheepResult + " " + wolfResult);
-    }
+	public static void main(String[] args) throws IOException {
 
-    private static void bfs(Point point) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(point);
-        isVisited[point.r][point.c] = true;
-        int wolfCnt = 0;
-        int sheepCnt = 0;
-        while (!queue.isEmpty()) {
-            Point point2 = queue.poll();
-            int r = point2.r;
-            int c = point2.c;
-            if (map[r][c] == 'k') {
-                sheepCnt++;
-            } else if (map[r][c] == 'v') {
-                wolfCnt++;
-            }
-            for (int i = 0; i < 4; i++) {
-                int r2 = point2.r + dx[i];
-                int c2 = point2.c + dy[i];
-                if ((r2 > 0 && r2 < R && c2 > 0 && c2 < C) && !isVisited[r2][c2] && map[r2][c2] != '#') {
-                    isVisited[r2][c2] = true;
-                    queue.offer(new Point(r2, c2));
-                }
-            }
-        }
-        if (sheepCnt > wolfCnt) {
-            sheepResult += sheepCnt;
-        } else {
-            wolfResult += wolfCnt;
-        }
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new char[N][M];
+		visit = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			char[] temp = br.readLine().toCharArray();
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = temp[j];
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] == '.' || visit[i][j])
+					continue;
+				bfs(i, j);
+			}
+		}
+		System.out.println(sheep + " " + wolf);
+	}
 
-    static class Point {
-        int r;
-        int c;
-
-        Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
+	private static void bfs(int i, int j) {
+		Queue<int[]> q = new ArrayDeque<int[]>();
+		q.offer(new int[] { i, j });
+		visit[i][j] = true;
+		int wolfCnt = 0;
+		int sheepCnt = 0;
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+			int x = now[0];
+			int y = now[1];
+			if (arr[x][y] == 'v')
+				wolfCnt++;
+			else if (arr[x][y] == 'k')
+				sheepCnt++;
+			for (int d = 0; d < 4; d++) {
+				int nx = x + dir[d][0];
+				int ny = y + dir[d][1];
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M || visit[nx][ny] || arr[nx][ny] == '#')
+					continue;
+				q.offer(new int[] { nx, ny });
+				visit[nx][ny] = true;
+			}
+		}
+		if (wolfCnt < sheepCnt) {
+			sheep += sheepCnt;
+		} else {
+			wolf += wolfCnt;
+		}
+	}
 
 }
