1e78a3068ca9a80b637cc5e8b756806395941e6a
BaekJeongEun <sophia2404@naver.com>
2023-10-05 21:57:56 +0900

    [BaekJoon]: 너비 우선 탐색 4(S2)

---
+++ CodingTest/src/BaekJoon/BaekJoon24447.java
@@ -0,0 +1,77 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BaekJoon24447 { // 너비 우선 탐색 4(S2)
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	
+	private static int N, M, R;
+	private static List<Integer> []list;
+	
+	public static void main(String[] args) throws IOException {
+		br=new BufferedReader(new InputStreamReader(System.in));
+		st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		R=Integer.parseInt(st.nextToken());
+		
+		list=new ArrayList[N+1];
+		for(int i=1;i<=N;i++) list[i]=new ArrayList<>();
+		
+		for(int i=0;i<M;i++) {
+			st=new StringTokenizer(br.readLine());
+			int u=Integer.parseInt(st.nextToken());
+			int v=Integer.parseInt(st.nextToken());
+			list[u].add(v);
+			list[v].add(u);
+		}
+		for(int i=1;i<=N;i++) Collections.sort(list[i]);
+		bfs(R);
+	}
+
+	private static void bfs(int x) {
+		Queue<Integer> queue=new LinkedList<>();
+		queue.add(x);
+		
+		boolean []visited=new boolean[N+1];
+		visited[x]=true;
+		
+		int cnt=0;
+		int dep=0;
+		long []order=new long[N+1];
+		long []depth=new long[N+1];
+		for(int i=1;i<=N;i++) depth[i]=-1;
+		while(!queue.isEmpty()) {
+			int size=queue.size();
+			
+			while(size-->0) {
+				int q=queue.poll();
+				cnt++;
+				order[q]=cnt;
+				depth[q]=dep;
+				
+				for(int i:list[q]) {
+					if(!visited[i]) {
+						visited[i]=true;
+						queue.add(i);
+					}
+				}
+			}
+			dep++;
+		}
+		long sum=0;
+		for(int i=1;i<=N;i++) {
+			sum+=depth[i]*order[i];
+		}
+		System.out.println(sum);
+	}
+}
