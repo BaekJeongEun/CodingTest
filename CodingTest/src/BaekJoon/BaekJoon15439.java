d8b47e87ad84bd5fe51cb58945bb4c0c05533746
BaekJeongEun <sophia2404@naver.com>
2023-10-27 16:48:40 +0900

    [BaekJoon]: 베라의 패션(B4)

--- CodingTest/src/BaekJoon/BaekJoon15439.java
+++ CodingTest/src/BaekJoon/BaekJoon15439.java
@@ -1,5 +1,9 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BaekJoon15439 { // 베라의 패션(B4)
 
 	public static void main(String[] args) throws IOException{
