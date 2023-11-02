98bf4cc4774102d087e61d97fb2b1588d449a739
BaekJeongEun <sophia2404@naver.com>
2023-11-02 21:47:01 +0900

    [BaekJoon]: 대칭 차집합 (S4)

--- CodingTest/src/BaekJoon/BaekJoon1269.java
+++ CodingTest/src/BaekJoon/BaekJoon1269.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class BaekJoon1269 { // 대칭 차집합 (S4)
+public class BaekJoon1269 { // 대칭 차집합(S4)
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
