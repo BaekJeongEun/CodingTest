3a366972a19cc8b539b151a8b3c1f4a49e202e47
BaekJeongEun <sophia2404@naver.com>
2023-11-11 17:56:15 +0900

    [BaekJoon]: 알고리즘의 수행 시간 1(B5)

---
+++ CodingTest/src/BaekJoon/BaekJoon24262.java
@@ -0,0 +1,10 @@
+package BaekJoon;
+
+public class BaekJoon24262 { // 알고리즘의 수행 시간 1(B5)
+
+	public static void main(String[] args) {
+		System.out.println(1);
+        System.out.println(0);
+	}
+
+}
