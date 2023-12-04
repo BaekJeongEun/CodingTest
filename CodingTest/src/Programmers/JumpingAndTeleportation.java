a43be765dd6ebad80496955b5da1288dd0e42a09
BaekJeongEun <sophia2404@naver.com>
2023-12-04 11:00:00 +0000

    [Programmers]: 점프와 순간 이동(L2)

--- CodingTest/src/Programmers/JumpingAndTeleportation.java
+++ CodingTest/src/Programmers/JumpingAndTeleportation.java
@@ -5,17 +5,18 @@ public class JumpingAndTeleportation { // 점프와 순간 이동(L2)
 	public static void main(String[] args) {
 		System.out.println(solution(5));
 	}
+
 	public static int solution(int n) {
-        int ans = 0;
-        while(n!=1){
-            if(n%2 == 0)
-                n /= 2;
-            else{
-                n -= 1;
-                ans ++;
-            }
-        }
-        ans ++;
-        return ans;
-    }
+		int ans = 0;
+		while (n != 1) {
+			if (n % 2 == 0)
+				n /= 2;
+			else {
+				n -= 1;
+				ans++;
+			}
+		}
+		ans++;
+		return ans;
+	}
 }
