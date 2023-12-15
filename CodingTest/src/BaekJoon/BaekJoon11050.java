8e4f91edb5a679602a50cb0d197368f40d5dc5cc
BaekJeongEun <sophia2404@naver.com>
2023-12-15 10:25:40 +0900

    [BaekJoon]: 이항 계수 1(B1)

---
+++ CodingTest/src/BaekJoon/BaekJoon11050.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon11050 { // 이항 계수 1(B1)
+public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+ 
+		System.out.println(BC(N, K));
+		
+	}
+	
+	static int BC(int n, int k) {
+		if(n == k || k == 0) {
+			return 1;
+		}
+		return BC(n - 1, k - 1) + BC(n - 1, k);
+	}
+}
