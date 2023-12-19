9b0a281ececb6551dd65ba9500b9564de6c00d74
BaekJeongEun <sophia2404@naver.com>
2023-12-19 23:46:37 +0900

    [BaekJoon]: 문자와 문자열(B5)

--- CodingTest/src/BaekJoon/BaekJoon27866.java
+++ CodingTest/src/BaekJoon/BaekJoon27866.java
@@ -7,14 +7,9 @@ import java.io.InputStreamReader;
 public class BaekJoon27866 { // 문자와 문자열(B5)
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String st = br.readLine();
 		int n = Integer.parseInt(br.readLine())-1;
-		
 		br.close();
-		
 		System.out.println(st.charAt(n));
-		
-		
 	}
 }
