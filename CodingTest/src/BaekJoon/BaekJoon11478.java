bfb07074de78ad06bd91b729e933a19593404239
BaekJeongEun <sophia2404@naver.com>
2024-03-05 19:22:50 +0900

    [BaekJoon]: 서로 다른 부분 문자열의 개수(S3)

--- CodingTest/src/BaekJoon/BaekJoon11478.java
+++ CodingTest/src/BaekJoon/BaekJoon11478.java
@@ -1,27 +1,29 @@
 package BaekJoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 
 public class BaekJoon11478 { // 서로 다른 부분 문자열의 개수(S3)
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		Set<String> set = new HashSet<>();
 
-	      String str = br.readLine();
-	      Set<String> set = new HashSet<>();
-
-	      for (int i = 0; i < str.length(); i++) {
-	         for (int j = i + 1; j <= str.length(); j++) {
-	            set.add(str.substring(i, j));
-	         }
-	      }
-
-	      System.out.println(set.size());
-	      
-	   }
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				set.add(str.substring(i, j));
+			}
+		}
+		bw.write(set.size() + "");
+        bw.flush();
+        bw.close();
+	}
 }
