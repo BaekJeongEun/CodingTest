03ee4ec40c6c040910b0a2ab151288b09daa6872
BaekJeongEun <sophia2404@naver.com>
2024-03-05 19:46:12 +0900

    [BaekJoon]: git

--- CodingTest/src/BaekJoon/BaekJoon11478.java
+++ CodingTest/src/BaekJoon/BaekJoon11478.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class BaekJoon11478 { // 서로 다른 부분 문자열의 개수(S3)
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
