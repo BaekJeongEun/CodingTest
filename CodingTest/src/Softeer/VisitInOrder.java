322fa9db03c268267a5b4f03f911d8d55d8825ba
BaekJeongEun <sophia2404@naver.com>
2024-04-01 00:06:27 +0900

    [Softeer]: 순서대로 방문하기(L3)

--- CodingTest/src/Softeer/VisitInOrder.java
+++ CodingTest/src/Softeer/VisitInOrder.java
@@ -8,14 +8,16 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class VisitInOrder { // 순서대로 방문하기(L3)
-	static int N, M, arr[][], dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
+	static int N, M, answer, arr[][], dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
 	static Order orders[];
+	static boolean visit[][];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N][N];
+		visit = new boolean[N][N];
 		orders = new Order[M];
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -29,46 +31,25 @@ public class VisitInOrder { // 순서대로 방문하기(L3)
 			int y = Integer.parseInt(st.nextToken())-1;
 			orders[i] = new Order(x, y);
 		}
-		System.out.println(bfs());
+		visit[orders[0].x][orders[0].y] = true;
+		dfs(orders[0].x, orders[0].y, 1);
+		System.out.println(answer);
 	}
-	private static int bfs() {
-		int answer = 0;
-		boolean temp[][] = new boolean[N][N];
-		Queue<Node> q = new ArrayDeque<>();
-		temp[orders[0].x][orders[0].y] = true;
-		q.offer(new Node(orders[0].x, orders[0].y, 0, temp));
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			int x = now.x;
-			int y = now.y;
-			int order = now.order+1;
-			boolean[][] visit = now.visit;
-			if(order == M && x == orders[order-1].x && y == orders[order-1].y) {
+	private static void dfs(int x, int y, int order) {
+		if(orders[order].x == x && orders[order].y == y) {
+			if(order == M-1) {
 				answer++;
+				return;
 			}
-			for(int d=0; d<4; d++) {
-				int nx = x + dir[d][0];
-				int ny = y + dir[d][1];
-				if(nx < 0 || ny < 0 || nx>=N || ny>=N || visit[nx][ny] || arr[nx][ny] == 1) continue;
-				visit[nx][ny] = true;
-				int nextOrder = order;
-				if(order<M && orders[order].x == nx && orders[order].y == ny) {
-					nextOrder++;
-				}
-				q.offer(new Node(nx, ny, nextOrder, visit));
-			}
+			dfs(x, y, order+1);
 		}
-		return answer;
-	}
-	public static class Node {
-		int x, y, order;
-		boolean visit[][];
-		public Node(int x, int y, int order, boolean visit[][]) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.order = order;
-			this.visit = visit;
+		for(int d=0; d<4; d++) {
+			int nx = x + dir[d][0];
+			int ny = y + dir[d][1];
+			if(nx<0 || ny<0 || nx>=N || ny>=N || visit[nx][ny] || arr[nx][ny] == 1) continue;
+			visit[nx][ny] = true;
+			dfs(nx, ny, order);
+			visit[nx][ny] = false;
 		}
 	}
 	public static class Order {
