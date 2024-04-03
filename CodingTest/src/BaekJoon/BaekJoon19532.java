603645d2091d32b8144ceacb162f99f5960a6207
BaekJeongEun <sophia2404@naver.com>
2024-04-03 23:46:19 +0900

    [FIX]: 수학은 비대면강의입니다

---
+++ CodingTest/src/BaekJoon/BaekJoon19532.java
@@ -0,0 +1,32 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon19532 { // 수학은 비대면강의입니다
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        
+        int x=-1, y=-1;
+        for(x=-999; x<=999; x++) {
+        	for(y=-999; y<=999; y++) {
+        		if(a*x + b*y == c && d*x + e*y ==f) {
+        			System.out.println(x+" "+y);
+        			return;
+        		}
+        	}
+        }
+	}
+
+}
