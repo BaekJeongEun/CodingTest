cb232bbebabb9a9b82b19b0cb56ea4fcfcb46e80
BaekJeongEun <sophia2404@naver.com>
2024-04-13 21:05:13 +0900

    [CodeTree]: 메이즈 러너

--- CodingTest/src/CodeTree/MazeRunner.java
+++ CodingTest/src/CodeTree/MazeRunner.java
@@ -3,222 +3,195 @@ package CodeTree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class MazeRunner { // 메이즈러너
-	static int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}}, map;
-	static int exitX, exitY, minX, minY, N, M, K, answer, startX, startY, endX, endY;
-	static ArrayList<Candidate>[][] cadidates;
+public class MazeRunner {
+	static int N, M, K;
+	static int[][] map, copy;
+	static int exitR, exitC, move = 0, turn = 0;
+	static int[] dr = new int[] { -1, 1, 0, 0 };
+	static int[] dc = new int[] { 0, 0, -1, 1 };
+
 	public static void main(String[] args) throws IOException {
+		init();
+
+		simulate();
+
+		System.out.println(move);
+		System.out.print(exitR + 1 + " ");
+		System.out.print(exitC + 1);
+	}
+
+	static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		map = new int[N+1][N+1];
-		cadidates = new ArrayList[N+1][N+1];
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				cadidates[i][j] = new ArrayList<>();
-			}
-		}
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=N; j++) {
+
+		map = new int[N][N];
+		copy = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			cadidates[x][y].add(new Candidate(x, y));
-		}
-		st = new StringTokenizer(br.readLine());
-		exitX = Integer.parseInt(st.nextToken());
-		exitY = Integer.parseInt(st.nextToken());
 
-		print();
-		while(K-- > 0 && M>0) {
-			// 이동
-			move();
-			//System.out.println("=============================================이동 후");
-			if(M<=0) break;
-			//print();
-			
-			// 정사각형 구하기
-			int n = getRectangle();	
-			
-			// 시계 90도 회전			
-			rotate(n);		
-			//System.out.println("=============================================회전 후");
-			//print();
-
-			
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int r = Integer.parseInt(st.nextToken()) - 1;
+			int c = Integer.parseInt(st.nextToken()) - 1;
+			map[r][c] -= 1;
 		}
-		sb.append(answer).append("\n").append(exitX).append(" ").append(exitY);
-		System.out.println(sb.toString());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		exitR = Integer.parseInt(st.nextToken()) - 1;
+		exitC = Integer.parseInt(st.nextToken()) - 1;
+		map[exitR][exitC] = 10;
 	}
-	
-	private static void print() {
-		System.out.println("----------------------------------- 내구성");
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(exitX ==i && exitY==j) 
-					System.out.print(". ");
-				else
-					System.out.print((map[i][j]<=0?0:map[i][j])+" ");
-			}System.out.println();
-		}
 
-		System.out.println("------------------------------------ 사람수 "+M);
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(exitX ==i && exitY==j) 
-					System.out.print(". ");
-				else
-				System.out.print(cadidates[i][j].size()+" ");
-			}System.out.println();
+	static void simulate() {
+		while (++turn <= K) {
+			allMove();
+
+			if (isDone())
+				break;
+
+			rotate();
 		}
 	}
 
-	private static void rotate(int n) {
-		// 회전하면서 내구도 1씩 깎기, cadidates도 돌리기..
-		int[][] temp = new int[N+1][N+1];
-		for(int i=1; i<=N; i++) {
-			temp[i] = map[i].clone();
-		}
-		ArrayList<Candidate> tempList[][] = new ArrayList[N+1][N+1];
-		for(int i=1; i<=N; i++) {
-			tempList[i] = cadidates[i].clone();
-		}
-		
-		System.out.println("시작 "+startX+" "+startY+"   : "+n);
-		System.out.println("끝점 "+endX+" "+endY+"   : "+n);
-		boolean change = false;
-		for(int j=startY, j2=startX; j<=endY; j++, j2++) {
-			for(int i=startX, i2=endY; i<=endX; i++, i2--) {
-				if(i == exitX && j == exitY && !change) {
-					exitX = j2;
-					exitY = i2;
-					change = true;
-					//System.out.println("출구   ~~~ "+j2+" "+i2);
-				}
-				//System.out.println(i+"|"+j);
-				//System.out.println((j2)+"|"+(i2));
-				//System.out.println("-----");
-				temp[j2][i2] = map[i][j]-1;
-				tempList[j2][i2] = cadidates[i][j];
-			}
-		}
-		map = temp;
-		cadidates = tempList;
-	}
-	private static int getRectangle() {
-		// 모든 참가자의 위치와 출구 위치의 최단 거리 구하기
-		PriorityQueue<Rectangle> rectangles = new PriorityQueue<>();
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(cadidates[i][j]==null || cadidates[i][j].isEmpty()) continue;
-				rectangles.offer(new Rectangle(i, j));
-			}
-		}
-		// 가장 작은 정사각형의 좌표
-		minX = rectangles.peek().x;
-		minY = rectangles.peek().y;
-		
-		int n = Math.max((Math.abs(exitX-minX)), (Math.abs(exitY-minY))) + 1; // 정사각형 변 길이 구하기
-		//System.out.println("exit "+exitX+" "+exitY);
-		//System.out.println("참가자 "+minX+" "+minY);
-		
-		A:for(int x=1; x<=N-n+1; x++) {
-			for(int y=1; y<=N-n+1; y++) {
-				int eX = x + n - 1;
-				int eY = y + n - 1;
-				if(x<=exitX && x<=minX && eX>=exitX && eX>=minX &&
-						y<=exitY && y<=minY && eY>=exitY && eY>=minY
-						) {
-					startX = x;
-					startY = y;
-					endX = eX;
-					endY = eY;
-					break A;
+	static void allMove() {
+		copyMap(map, copy);
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (isPerson(i, j)) {
+					move(i, j);
 				}
 			}
 		}
-		return n;
+
+		copyMap(copy, map);
 	}
-	private static void move() {
-		// 가상 temp에 모두 이동 시키기
-		ArrayList<Candidate>[][] temp = new ArrayList[N+1][N+1];
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				temp[i][j] = new ArrayList<>();
+
+	static void move(int r, int c) {
+		int dis = Math.abs(exitR - r) + Math.abs(exitC - c);
+
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			int nd = Math.abs(exitR - nr) + Math.abs(exitC - nc);
+
+			if (isValidPoint(nr, nc) && !isWall(nr, nc) && nd < dis) {
+
+				if (!isExit(nr, nc)) {
+					copy[nr][nc] += map[r][c];
+				}
+
+				copy[r][c] -= map[r][c];
+				move -= map[r][c];
+				break;
 			}
 		}
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				if(cadidates[i][j]==null || cadidates[i][j].isEmpty()) continue;
-				// 4방향 모두 확인해서 최단 거리 가까운 곳으로 이동
-				//System.out.println(exitX+" 출구는 ? "+exitY);
-				//System.out.println(i+" 현재 위치 ? "+j);
-				boolean moving = false;
-				int minDist = Math.abs(exitX-i)+Math.abs(exitY-j);
-				int minX = i, minY = j;
-				for(int d=0; d<4; d++) {
-					int nx = i + dir[d][0];
-					int ny = j + dir[d][1];
-					if(nx<1 || nx<1 || nx>=N+1 || ny>=N+1 || map[nx][ny] > 0) continue;
-					int currentDist = Math.abs(exitX-nx)+Math.abs(exitY-ny);
-					if(minDist <= currentDist) continue;
-					minX = nx;
-					minY = ny;
-					minDist = currentDist;
-					//System.out.println("d "+d +"  "+nx + "  "+ny+" currentDist "+ currentDist);
-					moving = true;
-				}
-				if(minX == exitX && minY == exitY) {
-					M -= cadidates[i][j].size();
-				}else {
-					for(Candidate c : cadidates[i][j])
-					temp[minX][minY].add(c);
-				}
-				if(moving) answer++;
+	}
+
+	static void copyMap(int[][] origin, int[][] target) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				target[i][j] = origin[i][j];
 			}
 		}
-		cadidates = temp;
 	}
-	static class Candidate {
-		int x, y;
 
-		public Candidate(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-		
+	static void rotate() {
+		int[] info = getRotateInfo();
+
+		rotate(info);
+
+		copyMap(copy, map);
 	}
-	static class Rectangle implements Comparable<Rectangle>{
-		int x, y;
 
-		public Rectangle(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
+	static int[] getRotateInfo() {
+		int r = -1;
+		int c = -1;
+		int size = 1;
+
+		while (true) {
+			for (int i = 0; i <= size; i++) {
+				r = exitR - size + i;
+				for (int j = 0; j <= size; j++) {
+					c = exitC - size + j;
+					if (isRotateAble(r, c, size)) {
+						return new int[] { r, c, size };
+					}
+				}
+			}
+
+			size += 1;
+		}
+	}
+
+	static boolean isRotateAble(int r, int c, int size) {
+		if (!isValidPoint(r, c) || !isValidPoint(r + size, c + size))
+			return false;
+
+		for (int i = 0; i <= size; i++) {
+			for (int j = 0; j <= size; j++) {
+				if (isPerson(r + i, c + j))
+					return true;
+			}
 		}
 
-		@Override
-		public int compareTo(Rectangle o) {
-			int myDist = (Math.abs(exitX-this.x) + Math.abs(exitY-this.y));
-			int yourDist = (Math.abs(exitX-o.x) + Math.abs(exitY-o.y));
-			if(myDist == yourDist) {
-				if(this.x == o.x) {
-					return this.y - o.y;
-				}return this.x - o.x;
-			}return myDist - yourDist;
+		return false;
+	}
+
+	static void rotate(int[] info) {
+		int r = info[0];
+		int c = info[1];
+		int size = info[2];
+		for (int i = 0; i <= size; i++) {
+			for (int j = 0; j <= size; j++) {
+				int value = map[r + i][c + j];
+				if (isWall(r + i, c + j)) {
+					value -= 1;
+				}
+				if (isExit(r + i, c + j)) {
+					exitR = r + j;
+					exitC = c + size - i;
+				}
+
+				copy[r + j][c + size - i] = value;
+			}
 		}
+	}
+
+	static boolean isDone() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (isPerson(i, j))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	static boolean isValidPoint(int r, int c) {
+		return 0 <= r && r < N && 0 <= c && c < N;
+	}
+
+	static boolean isWall(int r, int c) {
+		return 0 < map[r][c] && map[r][c] < 10;
+	}
+
+	static boolean isPerson(int r, int c) {
+		return map[r][c] < 0;
+	}
+
+	static boolean isExit(int r, int c) {
+		return map[r][c] == 10;
 	}
 }
