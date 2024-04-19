9ab89dcb720617778428acb0b869481cbe0d3f0c
BaekJeongEun <sophia2404@naver.com>
2024-04-19 20:41:35 +0900

    [BaekJoon]: 팰린드롬 만들기(S3)

--- CodingTest/src/BaekJoon/BaekJoon1213.java
+++ CodingTest/src/BaekJoon/BaekJoon1213.java
@@ -10,35 +10,37 @@ public class BaekJoon1213 { // 팰린드롬 만들기(S3)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = br.readLine();
 		int[] alpha = new int[26];
-		for(int i=0; i<line.length(); i++) {
-			int idx = line.charAt(i)-'A';
+		for (int i = 0; i < line.length(); i++) {
+			int idx = line.charAt(i) - 'A';
 			alpha[idx]++;
 		}
-		
-		int isOne =0;
-		for(int i=0; i<alpha.length; i++) {
-			if(alpha[i]%2!=0) isOne++;
+
+		int isOne = 0;
+		for (int i = 0; i < alpha.length; i++) {
+			if (alpha[i] % 2 != 0)
+				isOne++;
 		}
-		
-		String answer ="";
+
+		String answer = "";
 		StringBuilder sb = new StringBuilder();
-		if(isOne>1) answer += "I'm Sorry Hansoo";
+		if (isOne > 1)
+			answer += "I'm Sorry Hansoo";
 		else {
-			for(int i=0; i<alpha.length; i++) {
-				for(int r=0; r<alpha[i]/2; r++) {
-					sb.append((char)(i+65));
+			for (int i = 0; i < alpha.length; i++) {
+				for (int r = 0; r < alpha[i] / 2; r++) {
+					sb.append((char) (i + 65));
 				}
 			}
 			answer += sb.toString();
 			String end = sb.reverse().toString();
-			
+
 			sb = new StringBuilder();
-			for(int i=0; i<alpha.length; i++) {
-				if(alpha[i]%2==1) {
-					sb.append((char)(i+65));
+			for (int i = 0; i < alpha.length; i++) {
+				if (alpha[i] % 2 == 1) {
+					sb.append((char) (i + 65));
 				}
 			}
-			answer +=sb.toString()+end;
+			answer += sb.toString() + end;
 		}
 		System.out.println(answer);
 	}
