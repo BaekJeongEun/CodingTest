818b304aaf9650e3263f51ea8f9cceab048402e4
BaekJeongEun <sophia2404@naver.com>
2024-05-01 14:28:03 +0900

    [BaekJoon]: 카드 구매하기 2(S1)

---
+++ CodingTest/src/BaekJoon/BaekJoon16194.java
@@ -0,0 +1,32 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon16194 { // 카드 구매하기 2(S1)
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n + 1];
+        int[] d = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            d[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                d[i] = Math.min(d[i], d[i - j] + arr[j]);
+            }
+        }
+        
+        System.out.println(d[n]);
+	}
+
+}
