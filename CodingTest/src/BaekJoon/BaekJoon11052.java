6b9050161013d910dc951c660452ffda1334cd48
BaekJeongEun <sophia2404@naver.com>
2024-05-05 23:56:23 +0900

    [BaekJoon]: 카드 구매하기(S1)

--- CodingTest/src/BaekJoon/BaekJoon11052.java
+++ CodingTest/src/BaekJoon/BaekJoon11052.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BaekJoon11052 {
+public class BaekJoon11052 { // 카드 구매하기(S1)
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
