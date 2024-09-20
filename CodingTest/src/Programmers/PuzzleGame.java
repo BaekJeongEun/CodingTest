ce94afe968e3903d22f9047f69cbac6f222d99ff
BaekJeongEun <sophia2404@naver.com>
2024-09-20 21:05:44 +0900

    [add]: Puzzle Game

--- CodingTest/src/Programmers/PuzzleGame.java
+++ CodingTest/src/Programmers/PuzzleGame.java
@@ -1,10 +1,44 @@
 package Programmers;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class PuzzleGame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		int[] diffs = {1, 5, 3};
+		int[] times = {2, 4, 7};
+		
+		System.out.println(solution(diffs, times, 30));
 	}
 
+	public static long solution(int[] diffs, int[] times, long limit) { 
+        long left = diffs[0];       
+        long right = Arrays.stream(diffs).max().getAsInt();
+        long answer = Long.MAX_VALUE;
+        
+        while(left <= right) {
+        	long mid = (right+left)/2;
+        	long time = getTime(diffs, times, mid);
+        	if(time <= limit) { // 제한시간보다 작으면 숙련도 낮춰야 함
+        		right = mid - 1;
+        		answer = Math.min(answer, mid);
+        	} else {
+        		left = mid + 1;
+        	}
+        }
+        return answer;
+    }
+
+	private static long getTime(int[] diffs, int[] times, long level) {
+		long time = 0;
+		for(int i=0; i<diffs.length; i++) {
+			if(diffs[i]<=level) {
+				time = times[i];
+			}else {
+				time += ((times[i-1]+times[i]) * (diffs[i]-level) + times[i]);
+			}
+		}
+		return time;
+	}
 }
