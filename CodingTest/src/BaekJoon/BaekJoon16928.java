9ddd90732939bbf191444f4cfd3487e396f9d2c0
BaekJeongEun <sophia2404@naver.com>
2024-10-31 18:15:47 +0900

    [add]: 뱀과 사다리 게임(G5)

--- CodingTest/src/BaekJoon/BaekJoon16928.java
+++ CodingTest/src/BaekJoon/BaekJoon16928.java
@@ -3,58 +3,54 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BaekJoon16928 { // 뱀과 사다리 게임(G5)
-	private static int N, M;
-	private static int[] map, visited;
-
+	private static int N, M, min=Integer.MAX_VALUE;
+	private static HashMap<Integer, Integer> map;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visited = new int[101]; 
-
-		for (int i = 1; i <= 100; i++)
-			map[i] = i;
-		for (int i = 1; i <= 100; i++)
-			visited[i] = -1;
-		for (int i = 0; i < N; i++) {
+		map = new HashMap<>();
+		for(int i=0; i<N+M; i++) {
 			st = new StringTokenizer(br.readLine());
-			map[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			map.put(x, y);
 		}
-
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			map[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
-		}
-		bfs(1);
-		System.out.println(visited[100]);
+		bfs();
+		System.out.println(min);
 	}
 
-	public static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(start);
-		visited[start] = 0;
-
-		while (!queue.isEmpty()) {
-			int now = queue.poll();
-
-			for (int i = 1; i <= 6; i++) {
-				int moveTo = now + i;
-				if (moveTo > 100)
-					continue;
-				moveTo = map[moveTo];
-
-				if (visited[moveTo] == -1) {
-					visited[moveTo] = visited[now] + 1;
-					queue.offer(moveTo);
-				}
+	public static void bfs() {
+		PriorityQueue<int[]> q = new PriorityQueue<int[]>((a, b) -> a[2] - b[2]); // x, y, 횟스
+		q.offer(new int[] {0, 1, 0});
+		boolean[] visit = new boolean[101];
+		visit[1] = true;
+		while(!q.isEmpty()) {
+			int[] now = q.poll();
+			int x = now[0];
+			int y = now[1];
+			int num = 10*x+y;
+			int cnt = now[2];
+			if(num == 100) {
+				min = Math.min(min, cnt);
 			}
-
+			for(int d=1; d<=6; d++) {
+				int nextNum = num + d;
+				if(map.containsKey(nextNum)) {
+					nextNum = map.get(nextNum);
+				}
+				if(nextNum > 100 || visit[nextNum]) continue;
+				int nX = nextNum / 10;
+				int nY = nextNum % 10;
+				q.offer(new int[] {nX, nY, cnt+1});
+				visit[nextNum] = true;
+			}
 		}
 	}
 
